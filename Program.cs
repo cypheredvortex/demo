44dd50e2ab5e0eceadb0d1ef46d795749dc4d09e
mrxcode <alimalik21realms@gmail.com>
2025-10-23 19:56:42 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Personne
+{
+    public String nom { get; set; }
+    public String prenom { get; set; }
+    public int age { get; set; }
+    public Personne(String nom, String prenom, int age)
+    {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+    public override string ToString()
+    {
+        return $"Nom: {nom}, Prénom: {prenom}, Âge: {age}";
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Personne personne1 = new Personne("Dupont", "Jean", 30);
+        Personne personne2 = new Personne("Martin", "Sophie", 25);
+        Console.WriteLine(personne1.ToString());
+        Console.WriteLine(personne2.ToString());
+    }
+}
